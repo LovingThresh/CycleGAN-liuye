e958b004025e50e4446c248d1025b9719e32adb8
liuye <csu1704liuye@163.com>
2021-09-28 15:08:35 +0800

    add AttentionGAN_v1 Module add new G_Loss

--- module.py
+++ module.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import tensorflow_addons as tfa
 import tensorflow.keras as keras
+from tensorflow.keras.layers import *
 from tensorflow.keras.utils import plot_model
 
 
@@ -71,7 +72,13 @@ def ResnetGenerator(input_shape=(227, 227, 3),
     # 4
     for _ in range(n_downsamplings):
         dim //= 2
+        h = tf.pad(h, [[0, 0], [3, 3], [3, 3], [0, 0]], mode='REFLECT')
+        h = keras.layers.Conv2D(dim, 3, strides=1, padding='valid', use_bias=False)(h)
+        h = keras.layers.Conv2D(dim, 5, strides=1, padding='valid', use_bias=False)(h)
         h = keras.layers.Conv2DTranspose(dim, 3, strides=2, padding='same', use_bias=False)(h)
+        h = Norm()(h)
+        h = tf.nn.relu(h)
+        h = keras.layers.Conv2DTranspose(dim, 3, strides=1, padding='same', use_bias=False)(h)
         h = Norm()(h)
         h = tf.nn.relu(h)
 
@@ -79,19 +86,95 @@ def ResnetGenerator(input_shape=(227, 227, 3),
     h = tf.pad(h, [[0, 0], [3, 3], [3, 3], [0, 0]], mode='REFLECT')
     h = keras.layers.Conv2D(output_channels, 8, padding='valid')(h)
     # 假如我不添加tanh的话，又会出现报错
-    h = tf.tanh(h)
+    # h = tf.sigmoid(h)
     if attention:
-        attention_mask = tf.sigmoid(h[:, :, :, 1])
+        attention_mask = tf.sigmoid(h[:, :, :, 0])  # mask[0]
         # 上述式子对应的式attention_v2，在此次实验中可以发现裂缝都被涂上了绿色，可以以此来做无监督学习
         # 应该对上述式子进行更进一步的研究
         # attention_mask = tf.sigmoid(h[:, :, :, 0])  # 91
-        content_mask = h[:, :, :, 1:]
+        content_mask = tf.tanh(h[:, :, :, 1:])
         attention_mask = tf.expand_dims(attention_mask, axis=3)  # [93]
         attention_mask = tf.concat([attention_mask, attention_mask, attention_mask], axis=3)  # [94]
         h = content_mask * attention_mask + inputs * (1 - attention_mask)
-    # h = tf.tanh(h)
+    h = tf.tanh(h)
 
     return keras.Model(inputs=inputs, outputs=h)
+
+
+attention_mask, content_mask = None, None
+
+
+def AttentionCycleGAN_v1_Generator(input_shape=(227, 227, 3), output_channel=3,
+                                   n_downsampling=2, n_ResBlock=9,
+                                   norm='batch_norm', attention=False):
+    global attention_mask, content_mask
+    Norm = _get_norm_layer(norm)
+    a = keras.Input(shape=input_shape)
+    h = tf.pad(a, [[0, 0], [3, 3], [3, 3], [0, 0]], mode='REFLECT')
+
+    def model_layer_1(y):
+        y = Conv2D(64, (7, 7), (1, 1), 'valid')(y)
+        y = Norm()(y)
+        return y
+
+    h = model_layer_1(h)
+
+    n_downsampling = n_downsampling
+    n_ResBlock = n_ResBlock
+    if attention:
+        output_channel = output_channel + 1
+
+    for i in range(n_downsampling):
+        mult = 2 ** i
+
+        def model_layer_2(y):
+            y = Conv2D(64 * mult * 2, (3, 3), (2, 2), 'same')(y)
+            y = Norm()(y)
+            y = ReLU()(y)
+            return y
+
+        h = model_layer_2(h)
+
+    mult = 2 ** n_downsampling
+
+    for i in range(n_ResBlock):
+        x = h
+
+        def model_layer_3(y):
+            y = Conv2D(64 * mult, (3, 3), padding='valid')(y)
+            y = Norm()(y)
+            return y
+
+        h = tf.pad(h, [[0, 0], [1, 1], [1, 1], [0, 0]], mode='REFLECT')
+        h = model_layer_3(h)
+        h = ReLU()(h)
+        h = tf.pad(h, [[0, 0], [1, 1], [1, 1], [0, 0]], mode='REFLECT')
+        h = model_layer_3(h)
+
+        h = keras.layers.add([x, h])
+
+    upsampling = n_downsampling
+
+    for i in range(upsampling):
+        def model_layer_4(y):
+            y = Conv2DTranspose(64 * mult / 2, (3, 3), (2, 2), 'same')(y)
+            y = Norm()(y)
+            y = ReLU()(y)
+            return y
+        h = model_layer_4(h)
+        mult = mult / 2
+
+    h = tf.pad(h, [[0, 0], [3, 3], [3, 3], [0, 0]], mode='REFLECT')
+    h = Conv2D(output_channel, (8, 8), (1, 1), 'valid')(h)
+    result_layer = h
+    if attention:
+        attention_mask = tf.sigmoid(h[:, :, :, :1])
+        content_mask = h[:, :, :, 1:]
+        attention_mask = tf.concat([attention_mask, attention_mask, attention_mask], axis=3)
+        result_layer = content_mask * attention_mask + a * (1 - attention_mask)
+
+        return keras.Model(inputs=a, outputs=[result_layer, attention_mask, content_mask])
+    return keras.Model(inputs=a, outputs=result_layer)
 
 
 # model = ResnetGenerator((227, 227, 3), 3, attention=True)
@@ -148,7 +231,8 @@ class LinearDecay(keras.optimizers.schedules.LearningRateSchedule):
     def __call__(self, step):
         self.current_learning_rate.assign(tf.cond(
             step >= self._step_decay,
-            true_fn=lambda: self._initial_learning_rate * (1 - 1 / (self._steps - self._step_decay) * (step - self._step_decay)),
+            true_fn=lambda: self._initial_learning_rate * (
+                        1 - 1 / (self._steps - self._step_decay) * (step - self._step_decay)),
             false_fn=lambda: self._initial_learning_rate
         ))
         return self.current_learning_rate
--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import functools
+import os
 
 import cv2
 
@@ -13,6 +14,7 @@ import tqdm
 import matplotlib.pyplot as plt
 import data
 import module
+import tensorflow.keras.backend as K
 
 # ==============================================================================
 # =                                   param                                    =
@@ -23,7 +25,7 @@ py.arg('--datasets_dir', default='datasets')
 py.arg('--load_size', type=int, default=227)  # load image to this size
 py.arg('--crop_size', type=int, default=227)  # then crop to this size
 py.arg('--batch_size', type=int, default=1)
-py.arg('--epochs', type=int, default=8)
+py.arg('--epochs', type=int, default=5)
 py.arg('--epoch_decay', type=int, default=10)  # epoch to start decaying learning rate
 py.arg('--lr', type=float, default=0.0002)
 py.arg('--beta_1', type=float, default=0.5)
@@ -33,12 +35,16 @@ py.arg('--gradient_penalty_weight', type=float, default=10.0)
 py.arg('--cycle_loss_weight', type=float, default=10.0)
 py.arg('--identity_loss_weight', type=float, default=0.0)
 py.arg('--pool_size', type=int, default=50)  # pool size to store fake samples
+py.arg('--lambda_reg', type=float, default=1e-6)
+py.arg('--starting_rate', type=float, default=0.01, help='Set the lambda weight between GAN loss and '
+                                                         'Recon loss during curriculum period at the '
+                                                         'beginning. We used the 0.01 weight.')
 py.arg("--mode", default='client')
 py.arg("--port", default=52162)
 args = py.args()
 
 # output_dir
-output_dir = py.join('output', args.dataset+'attention_v2_validation_v3')
+output_dir = py.join('output', args.dataset + 'attention_v2_validation_v6')
 py.mkdir(output_dir)
 
 # save settings
@@ -68,10 +74,13 @@ A_B_dataset_test, _ = data.make_zip_dataset(
 # =                                   models                                   =
 # ==============================================================================
 
+G_A2B = module.AttentionCycleGAN_v1_Generator(input_shape=(args.crop_size, args.crop_size, 3), attention=True)
+G_B2A = module.AttentionCycleGAN_v1_Generator(input_shape=(args.crop_size, args.crop_size, 3), attention=True)
+
 # G_A2B = module.ResnetGenerator(input_shape=(args.crop_size, args.crop_size, 3), attention=True)
 # G_B2A = module.ResnetGenerator(input_shape=(args.crop_size, args.crop_size, 3))
-G_A2B = tf.keras.models.load_model(r'A2B.h5')
-G_B2A = tf.keras.models.load_model(r'B2A.h5')
+# G_A2B = tf.keras.models.load_model(r'A2B.h5')
+# G_B2A = tf.keras.models.load_model(r'B2A.h5')
 
 D_A = module.ConvDiscriminator(input_shape=(args.crop_size, args.crop_size, 3))
 D_B = module.ConvDiscriminator(input_shape=(args.crop_size, args.crop_size, 3))
@@ -79,7 +88,6 @@ D_B = module.ConvDiscriminator(input_shape=(args.crop_size, args.crop_size, 3))
 d_loss_fn, g_loss_fn = gan.get_adversarial_losses_fn(args.adversarial_loss_mode)
 cycle_loss_fn = tf.losses.MeanAbsoluteError()
 identity_loss_fn = tf.losses.MeanAbsoluteError()
-
 
 G_lr_scheduler = module.LinearDecay(args.lr, args.epochs * len_dataset, args.epoch_decay * len_dataset)
 D_lr_scheduler = module.LinearDecay(args.lr, args.epochs * len_dataset, args.epoch_decay * len_dataset)
@@ -94,12 +102,12 @@ D_optimizer = keras.optimizers.Adam(learning_rate=D_lr_scheduler, beta_1=args.be
 @tf.function
 def train_G(A, B):
     with tf.GradientTape() as t:
-        A2B = G_A2B(A, training=True)
-        B2A = G_B2A(B, training=True)
-        A2B2A = G_B2A(A2B, training=True)
-        B2A2B = G_A2B(B2A, training=True)
-        A2A = G_B2A(A, training=True)
-        B2B = G_A2B(B, training=True)
+        A2B, mask_B, temp_B = G_A2B(A, training=True)
+        B2A, mask_A, temp_A = G_B2A(B, training=True)
+        A2B2A = G_B2A(A2B, training=True)[0]
+        B2A2B = G_A2B(B2A, training=True)[0]
+        A2A = G_B2A(A, training=True)[0]
+        B2B = G_A2B(B, training=True)[0]
 
         A2B_d_logits = D_B(A2B, training=True)
         B2A_d_logits = D_A(B2A, training=True)
@@ -111,8 +119,20 @@ def train_G(A, B):
         A2A_id_loss = identity_loss_fn(A, A2A)
         B2B_id_loss = identity_loss_fn(B, B2B)
 
-        G_loss = (A2B_g_loss + B2A_g_loss) + (A2B2A_cycle_loss + B2A2B_cycle_loss) * args.cycle_loss_weight + (
-                    A2A_id_loss + B2B_id_loss) * args.identity_loss_weight
+        loss_reg_A = args.lambda_reg * (
+                K.sum(K.abs(mask_A[:, :-1, :, :] - mask_A[:, 1:, :, :])) +
+                K.sum(K.abs(mask_A[:, :, :-1, :] - mask_A[:, :, 1:, :])))
+
+        loss_reg_B = args.lambda_reg * (
+                K.sum(K.abs(mask_B[:, :-1, :, :] - mask_B[:, 1:, :, :])) +
+                K.sum(K.abs(mask_B[:, :, :-1, :] - mask_B[:, :, 1:, :])))
+
+        rate = args.starting_rate
+
+        G_loss = (A2B_g_loss + B2A_g_loss) * 0.5 + \
+                 (A2B2A_cycle_loss + B2A2B_cycle_loss) * args.cycle_loss_weight + \
+                 (A2A_id_loss + B2B_id_loss) * args.identity_loss_weight + \
+                 (loss_reg_A + loss_reg_B) * (1. - rate)
 
     G_grad = t.gradient(G_loss, G_A2B.trainable_variables + G_B2A.trainable_variables)
     G_optimizer.apply_gradients(zip(G_grad, G_A2B.trainable_variables + G_B2A.trainable_variables))
@@ -122,7 +142,9 @@ def train_G(A, B):
                       'A2B2A_cycle_loss': A2B2A_cycle_loss,
                       'B2A2B_cycle_loss': B2A2B_cycle_loss,
                       'A2A_id_loss': A2A_id_loss,
-                      'B2B_id_loss': B2B_id_loss}
+                      'B2B_id_loss': B2B_id_loss,
+                      'loss_reg_A': loss_reg_A,
+                      'loss_reg_B': loss_reg_B}
 
 
 @tf.function
@@ -163,10 +185,10 @@ def train_step(A, B):
 
 @tf.function
 def sample(A, B):
-    A2B = G_A2B(A, training=False)
-    B2A = G_B2A(B, training=False)
-    A2B2A = G_B2A(A2B, training=False)
-    B2A2B = G_A2B(B2A, training=False)
+    A2B = G_A2B(A, training=False)[0]
+    B2A = G_B2A(B, training=False)[0]
+    A2B2A = G_B2A(A2B, training=False)[0]
+    B2A2B = G_A2B(B2A, training=False)[0]
     return A2B, B2A, A2B2A, B2A2B
 
 
@@ -194,43 +216,74 @@ except Exception as e:
     print(e)
 
 # summary
-train_summary_writer = tf.summary.create_file_writer(py.join(output_dir, 'summaries', 'train'))
+training = True
+if training:
+    train_summary_writer = tf.summary.create_file_writer(py.join(output_dir, 'summaries', 'train'))
 
-# sample
-test_iter = iter(A_B_dataset_test)
-sample_dir = py.join(output_dir, 'samples_training')
-py.mkdir(sample_dir)
+    # sample
+    test_iter = iter(A_B_dataset_test)
+    sample_dir = py.join(output_dir, 'samples_training')
+    py.mkdir(sample_dir)
 
-# main loop
-with train_summary_writer.as_default():
-    for ep in tqdm.trange(args.epochs, desc='Epoch Loop'):
-        if ep < ep_cnt:
-            continue
+    # main loop
+    with train_summary_writer.as_default():
+        for ep in tqdm.trange(args.epochs, desc='Epoch Loop'):
+            if ep < ep_cnt:
+                continue
 
-        # update epoch counter
-        ep_cnt.assign_add(1)
+            # update epoch counter
+            ep_cnt.assign_add(1)
 
-        # train for an epoch
-        for A, B in tqdm.tqdm(A_B_dataset, desc='Inner Epoch Loop', total=len_dataset):
-            G_loss_dict, D_loss_dict = train_step(A, B)
+            # train for an epoch
+            for A, B in tqdm.tqdm(A_B_dataset, desc='Inner Epoch Loop', total=len_dataset):
+                G_loss_dict, D_loss_dict = train_step(A, B)
 
-            # # summary
-            tl.summary(G_loss_dict, step=G_optimizer.iterations, name='G_losses')
-            tl.summary(D_loss_dict, step=G_optimizer.iterations, name='D_losses')
-            tl.summary({'learning rate': G_lr_scheduler.current_learning_rate}, step=G_optimizer.iterations,
-                       name='learning rate')
+                # # summary
+                tl.summary(G_loss_dict, step=G_optimizer.iterations, name='G_losses')
+                tl.summary(D_loss_dict, step=G_optimizer.iterations, name='D_losses')
+                tl.summary({'learning rate': G_lr_scheduler.current_learning_rate}, step=G_optimizer.iterations,
+                           name='learning rate')
 
-            # sample
-            if G_optimizer.iterations.numpy() % 100 == 0:
-                A, B = next(test_iter)
-                A2B, B2A, A2B2A, B2A2B = sample(A, B)
-                img = im.immerge(np.concatenate([A, A2B, A2B2A, B, B2A, B2A2B], axis=0), n_rows=2)
-                im.imwrite(img, py.join(sample_dir, 'iter-%09d.jpg' % G_optimizer.iterations.numpy()))
+                # sample
+                sampling = False
+                if sampling:
+                    if G_optimizer.iterations.numpy() % 100 == 0:
+                        A, B = next(test_iter)
+                        A2B, B2A, A2B2A, B2A2B = sample(A, B)
+                        img = im.immerge(np.concatenate([A, A2B, A2B2A, B, B2A, B2A2B], axis=0), n_rows=2)
+                        # img = 0.9 * (img - 0.5 * (img.max() - img.min())) / (0.5 * (img.max() - img.min()))
+                        im.imwrite(img, py.join(sample_dir, 'iter-%09d.jpg' % G_optimizer.iterations.numpy()))
 
-        # save checkpoint
-        checkpoint.save(ep)
+            # save checkpoint
+            checkpoint.save(ep)
 
 # %%
+test = False
+if test:
+    G_A2B = checkpoint.checkpoint.G_A2B
+
+    mask_layer = G_A2B.layers[94].output
+    mask_model = tf.keras.Model(inputs=G_A2B.input, outputs=mask_layer)
+    path = r'C:\Users\liuye\Desktop\Slender Cracks Positive/'
+    for i in os.listdir(path):
+        filepath = path + i
+        o_img_file = cv2.imread(filepath)
+        img_file = ((o_img_file - 127.5) / 127.5).reshape((1, 227, 227, 3))
+        image = tf.convert_to_tensor(img_file, dtype=tf.float32)
+        mask = mask_model.predict(image)
+        # result = G_A2B.predict(image)
+        plt.imsave('./result_validation/{}.jpg'.format(i[:-4]), mask.reshape(227, 227))
+    # print(mask.shape)
+
+    # def plot_without_axis(plot, channel):
+    #     plt.imshow(plot.reshape(227, 227, channel))
+    #     plt.axis('off')
+    #     plt.xticks([])
+    #     plt.yticks([])
+    #     plt.show()
+    # plot_without_axis(img_file, 3)
+    # plot_without_axis(mask[0], 1)
+    # a = 1
 
 # A, B = next(test_iter)
 # A2B, B2A, A2B2A, B2A2B = sample(A, B)
@@ -250,7 +303,3 @@ with train_summary_writer.as_default():
 # # B2A2B2A = G_B2A(B2A2B, training=False)
 # img = im.immerge(np.concatenate([A2B2A], axis=0), n_rows=1)
 # im.imwrite(img, py.join(sample_dir, '0011a.jpg'))
-
-
-
-
